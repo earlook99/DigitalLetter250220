199d84701238f46fc7bbe5330d91df860328ccef
earlook99 <earlook99@gmail.com>
2025-02-22 12:09:12 +0900

    chore : AIController 사용을 위한 AIModule 추가

--- Source/DigitalLetter250220/DigitalLetter250220.Build.cs
+++ Source/DigitalLetter250220/DigitalLetter250220.Build.cs
@@ -8,7 +8,7 @@ public class DigitalLetter250220 : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AIModule" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
